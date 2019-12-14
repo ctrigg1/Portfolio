000a503a4316df7a0d9fe36baaa9707e1fdfbd77
ctrigg1 <courtneytriggs@ymail.com>
2019-12-14 00:38:17 -0600

    NASA updated 12.14.19

--- distro/assets/nasa.js
+++ distro/assets/nasa.js
@@ -1,34 +1,53 @@
 class NASA{
     constructor(){
-        this.key = '0eMgpF6lOC6NViYySXzRTHJREGKopSFLMpfHOLgt'
+        this.key = 'jtbtjVmZZcSxwHe2ogejLHk1nQI1MkvPD2hZ6aUY'
     }
     async GetData(date){
     
         const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=` + this.key);
         const data = await response.json();
-        const img = document.querySelector('#nasa');
-        img.setAttribute('src', data.url);
-       return data;
+        console.log(data);
+        return data
+       }
+       updateUI(data, vardisplay, vardescription, vartitle){
+        if(data.media_type === "image"){
+            img.setAttribute('src', data.url);
+            img.classList.remove("notactive")
+            vid.classList.add("notactive")
+        }
+        if (data.media_type === "video"){
+            vid.setAttribute('src', data.url);
+            vid.classList.remove("notactive")
+            img.classList.add("notactive")
+        }
+            vardescription.innerText = data.explanation;
+            console.log(data.explanation);
+            vardisplay.innerText = data.date;
+            vartitle.innerText = data.title;
        }
 }
 
 const userForm = document.querySelector('.searchDate');
 const userInput = document.querySelector('#input');
+const displayDate = document.querySelector('.displayDate');
+const displayTitle = document.querySelector('.displayTitle');
+const description = document.querySelector('.description');
+const img = document.querySelector('#nasaPhoto');
+const vid = document.querySelector('#nasaVideo');
+const nasaPic = new NASA();
 const newDate = new Date(); 
 const currentDate = newDate.getFullYear() + "-" + newDate.getMonth() + "-" + newDate.getDay();
 
 console.log(currentDate);
 
-const nasaPic = new NASA();
-
 nasaPic.GetData(currentDate)
-.then(data => console.log('pic received'))
+.then(data => nasaPic.updateUI(data, displayDate, description, displayTitle))
 .catch(err => console.log(err))
 
 userForm.addEventListener('submit' , e => {
     e.preventDefault();
     console.log(userInput.value);
     nasaPic.GetData(userInput.value)
-        .then(data => console.log('pic received'))
+        .then(data => nasaPic.updateUI(data, displayDate, description, displayTitle))
         .catch(err => console.log(err))
 })
